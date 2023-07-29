d68f796da141da5dba02e9c40c4578512f415860
Yigoya <123858017+Yigoya@users.noreply.github.com>
2023-07-29 20:41:29 +0300

    Add Atm Class.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,3 +6,67 @@
 #include <string.h>
 #include <unistd.h>
 using namespace std;
+
+// Class ATM to get user details
+class atm {
+private:
+    char username[30];
+    int password;
+    int balance;
+
+public:
+    char* usernames(void)
+    {
+        // Return username
+        return username;
+    }
+
+    int passwords(void)
+    {
+        // Return the password
+        return password;
+    }
+
+    // Function to get the data
+    void getData(void)
+    {
+        cin.ignore(
+            numeric_limits<streamsize>::max(),
+            '\n');
+        cout << "\nEnter username:";
+
+        cin.getline(username, 30);
+        cout << "\nEnter 4-digit "
+             << "password:";
+
+        cin >> password;
+        cin.ignore(
+            numeric_limits<streamsize>::max(),
+            '\n');
+        cout << "\nEnter initial"
+             << " balance:";
+
+        cin >> balance;
+        cin.ignore(
+            numeric_limits<streamsize>::max(),
+            '\n');
+    }
+
+    // Function displaying the data
+    void showData(void)
+    {
+        cout << "Username:" << username
+             << ", Password:" << password
+             << ", Balance:" << balance
+             << endl;
+    }
+
+    // Member Functions
+    int adduser();
+    int viewallusers();
+    int deleteuser(char*);
+    void updateuserasdeposit(char*);
+    void updateuseraswithdraw(char*);
+    int searchspecificuser(char*, int);
+    int searchallusertodisplay(char*);
+};
